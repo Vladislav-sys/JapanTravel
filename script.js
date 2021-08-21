447cea7a7256b9487fb4389261d5ffb673531240
Vlad Bugakov <74979119+Vladislav-sys@users.noreply.github.com>
2021-08-22 00:11:37 +0300

    Загружает файлы сайта

---
+++ script.js
@@ -0,0 +1,6 @@
+let  burger = document.querySelector(".header-burger")
+
+burger.onclick = function() {
+  burger.classList.toggle('active');
+  burger.previousElementSibling.classList.toggle("active");
+};
