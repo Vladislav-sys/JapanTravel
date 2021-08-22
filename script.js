af27aa6b250ae10c1ff954d87af1ad2eeab93b7b
Vlad Bugakov <74979119+Vladislav-sys@users.noreply.github.com>
2021-08-22 21:07:57 +0300

    Убирает скролл при открытом бургере

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ let  burger = document.querySelector(".header-burger")
 burger.onclick = function() {
   burger.classList.toggle('active');
   burger.previousElementSibling.classList.toggle("active");
+  document.body.classList.toggle("lock");
 };
